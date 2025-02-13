05f28794c4ae653b89e8b8a25238eab64a1e9500
Felipe 'Revz' Santos <fmsantos1997@hotmail.com>
2025-02-12 23:33:35 -0300

    feat: add golang demo tests

--- timer-api/timer/timer.go
+++ timer-api/timer/timer.go
@@ -1,8 +1,20 @@
 package timer
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 type Response struct {
-	Hostname string    `json:"hostname"`
-	Time     time.Time `json:"time"`
+	Hostname string `json:"hostname"`
+	Time     string `json:"time"`
+}
+
+func (r *Response) GetTime() Response {
+	currentDate := time.Now().Format("2006-01-02")
+
+	return Response{
+		Hostname: os.Getenv("HOSTNAME"),
+		Time:     currentDate,
+	}
 }
